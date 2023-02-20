e7a8db28ba16a739b6afe12f84586ba07152c42c
Kate-Lesnyak <catharinalesnyak@gmail.com>
2023-02-20 13:04:37 +0200

    Update FriendListItem.jsx

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -12,7 +12,7 @@ export const FriendListItem = ({ friend: { avatar, name, isOnline } }) => {
   );
 };
 
-Friend.propTypes = {
+FriendListItem.propTypes = {
   friend: PropTypes.shape({
     avatar: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
